4544343e9ba68c1b328039ee22144c192c8c3d30
Arman <arman1231@yandex.ru>
2022-10-13 15:30:46 +0300

    fix fonts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
 import Main from '../components/Main'
 import styles from '../styles/Home.module.css'
 
